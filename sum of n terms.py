1391c673c281aadf1cc9ef1c15600ea422281479
Kanika Rastogi <142605532+kanika05102004@users.noreply.github.com>
2023-08-22 12:37:55 +0530

    Add files via upload

--- sum of n terms.py
+++ sum of n terms.py
@@ -1,11 +1,12 @@
-n = 5
+n= 5
 # first number of sequence
+start = 2
 sum_seq = 0
 
 # run loop n times
 for i in range(0, n):
-    print(2, end="+")
-    sum_seq += 2
+    print(start, end="+")
+    sum_seq += start
     # calculate the next term
-    start = 2* 10 + 2
+    start = start * 10 + 2
 print("\nSum of above series is:", sum_seq)
